c8a4165eeaddcac591538198a33c1626cdd2df75
NITHISH-2207 <nithishs.24it@kongu.edu>
2026-01-20 19:53:19 +0530

    Update NullSafety.kt

--- NullSafety.kt
+++ NullSafety.kt
@@ -1,9 +1,11 @@
-fun profile(name : String?,age : Int?,mail: String?){
-    println("DETAILS: ");
-    println("Name: ${name ?: "Name is not entered"}");
-    println("Age: ${age ?: "age is not entered"}");
-    println("E-mail: ${mail ?: "mail is not entered"}");
-}
+class NullSafety{
+    fun profile(name : String?,age : Int?,mail: String?){
+        println("DETAILS: ");
+        println("Name: ${name ?: "Name is not entered"}");
+        println("Age: ${age ?: "age is not entered"}");
+        println("E-mail: ${mail ?: "mail is not entered"}");
+}}
 fun main(){
-    profile("NITHISH",20,null);
+    val obj=NullSafety()
+    obj.profile("NITHISH",20,null)
 }
